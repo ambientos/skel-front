804355523aeea516c5360678aee9bf55e37016f0
Anton Grakhov <ant.grakhov@gmail.com>
2020-02-28 19:49:58 +0500

    Init

---
+++ README.md
@@ -0,0 +1,32 @@
+# Skeleton front
+
+Include:
+
+- Babel
+- Webpack
+- browser-sync
+- gulp
+- gulp-autoprefixer
+- gulp-notify
+- gulp-plumber
+- gulp-pug
+- gulp-sass
+- gulp-sourcemaps
+
+To start the project you need to install npm packages:
+
+```sh
+$ yarn
+```
+
+To run the project:
+
+```sh
+$ gulp
+```
+
+To build js into production:
+
+```sh
+$ yarn webpack-build
+```
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+let path = require('path'),
+	webpack = require('webpack'),
+	srcPath = '/src/js',
+	distPath = '/dist/js'
+
+module.exports = {
+	mode: 'production',
+	context: __dirname + srcPath,
+	entry: {
+		site: './site/index.js'
+	},
+	output: {
+		filename: '[name].build.js',
+		path: path.resolve(__dirname) + distPath
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env']
+				}
+			}
+		}]
+	}
+}
